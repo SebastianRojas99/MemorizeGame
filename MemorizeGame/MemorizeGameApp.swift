13da58331da7a13a348734189de203b29324da22
Sebastian <96402832+SebastianRojas99@users.noreply.github.com>
2024-02-25 20:57:10 -0300

    Initial Commit

---
+++ MemorizeGame/MemorizeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeGameApp.swift
+//  MemorizeGame
+//
+//  Created by Sebastian Marquez Rojas on 25/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
