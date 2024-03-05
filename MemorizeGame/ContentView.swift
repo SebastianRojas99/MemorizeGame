8a474367ea5f7c793005b317d24ec4fe3dc5ade1
Sebastian <96402832+SebastianRojas99@users.noreply.github.com>
2024-03-04 23:48:23 -0300

    more view changes

--- MemorizeGame/ContentView.swift
+++ MemorizeGame/ContentView.swift
@@ -8,8 +8,8 @@
 import SwiftUI
 
 struct EmojiMemoryGameView: View {
-     var emojisArray: Array<String> = ["🐍","🐘","🦎","🪱","🐊","🐥","🦫","🐵","🐏","🦧","🦄"]
-     var emojiGame:EmojiMemoryGame = EmojiMemoryGame()
+    
+    var emojiGame:EmojiMemoryGame = EmojiMemoryGame()
     
     var body: some View {
 
@@ -20,8 +20,8 @@ struct EmojiMemoryGameView: View {
     
     var cards:some View{
         LazyVGrid(columns: [GridItem(.adaptive(minimum: 85))]) {
-            ForEach(emojisArray.indices,id: \.self){index in
-                CardView(content:emojisArray[index])
+            ForEach(emojiGame.cards.indices,id: \.self){index in
+                CardView(card: emojiGame.cards[index])
                     .aspectRatio(2/3,contentMode: .fit)
             }
         }.padding()
@@ -30,25 +30,26 @@ struct EmojiMemoryGameView: View {
     
    
     struct CardView:View {
-        var content:String
-        @State var isFaceUp = true
+        let card:MemoryGame<String>.Card
+        
+        
         var body: some View {
             ZStack{
                 let base = RoundedRectangle(cornerRadius: 12)
                 Group{
                     base.foregroundStyle(Color.white)
                     base.strokeBorder(lineWidth: 2)
-                    Text(content).font(.largeTitle)
+                    Text(card.content).font(.largeTitle)
                         .imageScale(.large)
                         .foregroundStyle(.tint)
                     
                 }
-                .opacity(isFaceUp ? 1 : 0)
-                base.fill().opacity(isFaceUp ?  0 : 1)
+                .opacity(card.isFaceUp ? 1 : 0)
+                base.fill().opacity(card.isFaceUp ?  0 : 1)
+            }.onTapGesture {
+                
             }
-            .onTapGesture {
-                isFaceUp.toggle()
-            }
+            
         }
     }
 }
